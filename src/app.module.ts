b955e813c41c865d228dc7986adc738de81ce692
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-06-18 13:56:15 +0900

    add static file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { MarginsModule } from './margins/margins.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import * as typeormConfig from './typeorm.config';
+
+import { MarginsModule } from './margins/margins.module';
+import { join } from 'path';
+
+
 
 @Module({
   imports: [
     MarginsModule,
-    TypeOrmModule.forRoot(typeormConfig)
+    TypeOrmModule.forRoot(typeormConfig),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    })
   ],
 })
 export class AppModule {}
