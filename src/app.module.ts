dbe2824c5d467fe1593dfabb056c36c6922eb451
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-06-18 14:15:39 +0900

    scheduler 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ScheduleModule } from '@nestjs/schedule'
 
 import * as typeormConfig from './typeorm.config';
 
 import { MarginsModule } from './margins/margins.module';
+import { TasksModule } from './tasks/tasks.module';
 import { join } from 'path';
 
 
@@ -12,7 +14,9 @@ import { join } from 'path';
 @Module({
   imports: [
     MarginsModule,
+    TasksModule,
     TypeOrmModule.forRoot(typeormConfig),
+    ScheduleModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
     })
