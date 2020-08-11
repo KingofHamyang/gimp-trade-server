3959aadacd650ae95c03e34940223c764acb87a9
shwjdgh34 <nossi8128@gmail.com>
2020-08-11 16:24:40 +0900

    Cerate new migration for new margin entity

--- src/margins/margin.entity.ts
+++ src/margins/margin.entity.ts
@@ -5,12 +5,18 @@ export class Margin {
   @PrimaryColumn()
   datetime: Date;
 
-  @Column("decimal", { precision: 5, scale: 2 })
+  @Column('decimal', { precision: 9, scale: 0 })
   upbit_price: number;
 
-  @Column("decimal", { precision: 5, scale: 2 })
+  @Column('decimal', { precision: 7, scale: 1 })
   bitmex_price: number;
 
-  @Column("decimal", { precision: 5, scale: 2 })
-  rate: number;
+  @Column('decimal', { precision: 6, scale: 3 })
+  fixed_gimp: number;
+
+  @Column('decimal', { precision: 6, scale: 3 })
+  gimp: number;
+
+  @Column('decimal', { precision: 6, scale: 3 })
+  usdkrw_rate: number;
 }
