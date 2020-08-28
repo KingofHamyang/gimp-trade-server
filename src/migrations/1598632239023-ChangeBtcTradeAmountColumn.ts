7ec9bf661bc4bb00fd1a49b8047bbe1fcf75d606
shwjdgh34 <nossi8128@gmail.com>
2020-08-29 01:31:12 +0900

    Update migrations

--- src/migrations/1598632239023-ChangeBtcTradeAmountColumn.ts
+++ src/migrations/1598632239023-ChangeBtcTradeAmountColumn.ts
@@ -1,16 +1,14 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class UpdateBtcTradeAmountScales1598612121803 implements MigrationInterface {
-    name = 'UpdateBtcTradeAmountScales1598612121803'
+export class ChangeBtcTradeAmountColumn1598632239023 implements MigrationInterface {
+    name = 'ChangeBtcTradeAmountColumn1598632239023'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "trade_log" ALTER COLUMN "btc_trade_amount" TYPE numeric(9,5)`);
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "btc_trade_amount" TYPE numeric(9,5)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "btc_trade_amount" TYPE numeric(9,0)`);
-        await queryRunner.query(`ALTER TABLE "trade_log" ALTER COLUMN "btc_trade_amount" TYPE numeric(9,0)`);
     }
 
 }
