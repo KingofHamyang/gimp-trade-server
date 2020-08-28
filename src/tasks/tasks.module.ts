866db8bb689315a2fa0653c6db1dfeca23b5d40d
shwjdgh34 <nossi8128@gmail.com>
2020-08-28 21:25:08 +0900

    Add TradeLogs modules in Tasks module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
 import { UsersService } from '../users/users.service'
 import { GimpsService } from '../gimps/gimps.service'
+import { TradeLogsService } from '../trade-logs/trade-logs.service'
 
 import { User } from '../users/user.entity'
 import { Gimp } from '../gimps/gimp.entity'
+import { TradeLog } from '../trade-logs/trade-log.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Gimp])],
-  providers: [TasksService, UsersService, GimpsService],
+  imports: [TypeOrmModule.forFeature([User, Gimp, TradeLog])],
+  providers: [TasksService, UsersService, GimpsService, TradeLogsService],
 })
 export class TasksModule {}
